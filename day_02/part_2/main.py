0b20ee13bfbb77f4e9fdb58f232c0c1beecb1a83
Moritz Strenger <moritz.strenger@gmail.com>
2021-12-02 21:05:46 +0100

    Solve day 2

---
+++ day_02/part_2/main.py
@@ -0,0 +1,28 @@
+import fire
+
+
+def main():
+    with open('input.txt') as f:
+        data = [x.split() for x in f.read().splitlines()]
+
+    horizontal_pos = 0
+    depth = 0
+    aim = 0
+
+    for command, value in data:
+        value = int(value)
+        if command == 'up':
+            aim -= value
+        elif command == 'down':
+            aim += value
+        elif command == 'forward':
+            horizontal_pos += value
+            depth += aim * value
+        else:
+            raise ValueError(f"Unexpected command {command}")
+
+    print(horizontal_pos * depth)
+
+
+if __name__ == '__main__':
+    fire.Fire(main)
